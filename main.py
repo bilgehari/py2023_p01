1d59ec480766877da9e6d9872ceaae0993c01361
bilgehari <bilgeekayalak@gmail.com>
2023-11-15 20:45:56 +0300

    ilk ödevim

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import os  # işletim sistemi etkileşimi için dizin yolları vs.
 import pdfplumber  # PDF okumak için
 from fpdf import FPDF  # PDF oluşturmak için.
+import pdftitle
+print(pdftitle.get_title_from_file(pdfler/bilgi2.pdf))
+
+
+#Grid Sistemi
+from tkinter import *
+
+pencere = Tk()
+pencere.title("Grid Uygulaması")
+
+baslik = Label(pencere, text="Bilge Hari'nin Projesi")
+etiket1 = Label(pencere, text="[ Birinci satır, birinci sütun ]")
+etiket2 = Label(pencere, text="[ Birinci satır, ikinci sütun ]")
+etiket3 = Label(pencere, text="[ İkinci satır, birinci sütun ]")
+etiket4 = Label(pencere, text="[ İkinci satır, ikinci sütun ]")
+buton1 = Button(pencere, text="<<< Oku >>>", fg="red", bg="green")
+buton2 = Button(pencere, text="<<< Kaydet >>>", fg="black", bg="green")
+buton3 = Button(pencere, text="<<< Çıkış >>>", fg="black", bg="red", command=pencere.quit)
+
+baslik.grid(row=0, columnspan=2, padx=50, pady=25)
+baslik.config(font=("Times New Roman", 30))
+etiket1.grid(row=1, column=0, padx= 10, pady=20)
+etiket2.grid(row=1, column=1, padx= 10, pady=20)
+etiket3.grid(row=2, column=0, padx= 10, pady=20)
+etiket4.grid(row=2, column=1, padx= 10, pady=20)
+buton1.grid(row=3, column=0, padx=50, pady=10)
+buton2.grid(row=3, column=1, padx=50, pady=10)
+buton3.grid(row=4, columnspan=2, padx=5, pady=10)
+
+pencere.mainloop()
+
 
 
 def menu():
